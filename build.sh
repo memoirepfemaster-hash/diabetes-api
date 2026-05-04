cce0841821234c4cd836437a1a819168fb01392b
Kouki <kouki@gmail.com>
2026-05-04 01:02:23 +0100

    Simplify build.sh for debugging

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-set -e
-pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
